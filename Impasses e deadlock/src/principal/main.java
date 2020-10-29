88f2e1eaa656b1be96562a7975fd0b30528fbeb7
Bruno-Vasconcelos <44508347+Bruno-Vasconcelos@users.noreply.github.com>
2020-10-29 10:55:45 -0300

    update main

--- Impasses e deadlock/src/principal/main.java
+++ Impasses e deadlock/src/principal/main.java
@@ -22,32 +22,30 @@ public class main {
 			int nProcessos = Integer.parseInt(primeiraLinha[1]);
 
 			for (int i = 0; i < nLinhas; i++) {
-//deu certo
 				linha = lerArq.readLine();
 				processos = linha.split(";");
 				processoP.add(processos[0]); // possuido
 				processoR.add(processos[1]); // requisitado
 			}
-			
-			//int a = 0;
+
 			for (int i = 0; i < nLinhas; i++) {
 				System.out.println((i + 1) + " quer: " + processoR.get(i));
 
 				for (int j = 0; j < nLinhas; j++) {
 					if (processoR.get(i).length() == 2) { // se não tem dois requisitos
-						System.out.println("   "+(j + 1) + " tem: " + processoP.get(j));
 						if (processoR.get(i).trim().equals(processoP.get(j).trim())) {
-							i = j;
-							ListaAux.add(i);
+							System.out.println("   " + (j + 1) + " tem: " + processoP.get(j));
+							i = j - 1;
+							ListaAux.add(i+2);
 							break;
 						}
 					} else { // se tem dois ou mais requisitos
 						int qtdR = processoR.get(i).length(), inicio = 0, fim = 2;
 						for (int z = 0; z < qtdR / 2; z++) {
-							System.out.println("   "+(j + 1) + " tem: " + processoP.get(j));
-							if (processoR.get(i).substring(inicio,fim).trim().equals(processoP.get(j).trim())) {
-								i = j;
-								ListaAux.add(i);
+							System.out.println("   " + (j + 1) + " tem: " + processoP.get(j));
+							if (processoR.get(i).substring(inicio, fim).trim().equals(processoP.get(j).trim())) {
+								i = j - 1;
+								ListaAux.add(i+2);
 								break;
 							}
 							inicio += 2;
@@ -55,11 +53,10 @@ public class main {
 						}
 					}
 				}
-				/*System.out.println(countDuplicates(ListaAux));
-				if(a > countDuplicates(ListaAux)) {
+				System.out.println(countDuplicates(ListaAux));
+				if (countDuplicates(ListaAux)) {
 					break;
 				}
-				a++;*/
 			}
 			System.out.println(ListaAux.toString());
 
@@ -67,24 +64,21 @@ public class main {
 		} catch (IOException e) {
 			System.err.printf("Não foi possível abrir o arquivo.\n" + e.getMessage());
 		}
-		
+
 	}
-	
-    public static int countDuplicates(ArrayList<Integer> list) 
-    { 
-    	int nRepete = 0;
-        // Create a new ArrayList 
-        ArrayList<Integer> newList = new ArrayList<Integer>(); 
-  
-        // Traverse through the first list 
-        for (int element : list) { 
-            if (!newList.contains(element)) { 
-            	nRepete++;
-            } 
-        } 
-  
-        // return the new list 
-        return nRepete; 
-    } 
+
+	public static Boolean countDuplicates(ArrayList<Integer> list) {
+		int nRepete = 0;
+		// Traverse through the first list
+		for (int element : list) {
+			if (list.contains(element)) {
+				nRepete++;
+			}
+		}
+		if (nRepete > 2) {
+			return true;
+		} else
+			return false;
+	}
 
 }
